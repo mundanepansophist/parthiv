068ff9846697fdf510112c7109026517cb6bef01
parthiv_k <themundanepansophist@gmail.com>
2016-08-28 15:35:40 +0530

    added the cv/profile section

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-$(document).ready(function(){
-
-   $( window ).resize(function() {
-    if(window.innerWidth <= 1355/2){
-        $(".profile").css("flex-direction","column");
-    }
-});
-
+$(document).ready(function () {
+    $(window).resize(function () {
+        if (window.innerWidth <= 1355 / 2) {
+            $(".profile").css("flex-direction", "column");
+        }
+    });
+    $('#profile').click(function () {
+        $(this).addClass('prof-a-sel');
+        $("#prof-a").css({
+            'color': '#1abc9c'
+        });
+        $('#cv').removeClass('prof-a-sel');
+        $("#cv-a").css({
+            'color': 'black'
+        });
+    });
+    $('#cv').click(function () {
+        $(this).addClass('prof-a-sel');
+        $('#cv-a').css({
+            'color': '#1abc9c'
+        });
+        $('#profile').removeClass('prof-a-sel');
+        $("#prof-a").css({
+            'color': 'black'
+        });
+    });
 });
